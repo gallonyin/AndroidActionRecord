65e98f3a38e9b777ef8f4efffeded5cfd0411e9f
gallonyin <gallonyin@gmail.com>
2019-04-05 22:51:53 +0800

    update hex2dec

---
+++ app/src/main/java/com/gallon/actionrecord/util/TranslateUtil.kt
@@ -0,0 +1,38 @@
+package com.gallon.actionrecord.util
+
+import android.os.SystemClock
+import com.gallon.actionrecord.model.ActionUnit
+import java.math.BigInteger
+
+/**
+ * Created by Gallon2 on 2019/4/5.
+ */
+object TranslateUtil {
+
+    /**
+     * 使用该动作时调用，插入事件使用时间
+     * @param actionUnitList
+     */
+    fun refreshActionTime(actionUnitList: List<ActionUnit>) {
+        if (actionUnitList.isEmpty()) return
+        val doneTime = actionUnitList[0].actionTime
+        val now = SystemClock.uptimeMillis()
+        for (actionUnit in actionUnitList) {
+            actionUnit.actionTime = actionUnit.actionTime - doneTime + now
+        }
+    }
+
+    /**
+     * 十六进制转换十进制
+     */
+    fun hexToDec(str: String?): String? {
+        val strArr = str?.split(" ")
+        if (strArr?.size == 3) {
+            return BigInteger(strArr[0], 16).toString(10) + " " +
+                    BigInteger(strArr[1], 16).toString(10) + " " +
+                    BigInteger(strArr[2], 16).toString(10)
+        }
+        return null
+    }
+
+}
